18326be42efa31f67107bf9f3205662bd9e1fa8f
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2023-12-19 16:32:11 +0100

    zsh

--- hosts/crazy/configuration.nix
+++ hosts/crazy/configuration.nix
@@ -83,6 +83,12 @@
     inputs.sddm-sugar-catppuccin.packages.${pkgs.system}.default
   ];
 
+  programs = {
+    zsh = {
+      enable = true;
+    }
+  }
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
