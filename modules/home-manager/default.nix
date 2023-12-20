13fbf5dae08587e7570fd8bb636c54b7d0af65cd
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2023-12-20 11:35:32 +0100

    reset

--- modules/home-manager/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  fonts = import ./fonts.nix;
-  monitors = import ./monitors.nix;
-  shellcolor = import ./shellcolor.nix;
-  wallpaper = import ./wallpaper.nix;
-  xdg-portal = import ./xdg-portal.nix;
-}
