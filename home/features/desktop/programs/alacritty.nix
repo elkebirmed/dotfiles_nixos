2e893e6212c251f33024f695b5d17151cd9e1f65
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2023-12-21 15:01:28 +0100

    ...

--- home/features/desktop/programs/alacritty.nix
+++ home/features/desktop/programs/alacritty.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, config, lib, ... }: {
   programs.alacritty = {
     enable = true;
-  }
+  };
   
   home.sessionVariables.EDITOR = "${config.programs.alacritty.package}/bin/alacritty";
 }
