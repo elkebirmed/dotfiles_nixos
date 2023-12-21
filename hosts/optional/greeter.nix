f0358c59b5fab1d745b7afba40cef47e48289fac
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2023-12-21 10:07:09 +0100

    ---

--- hosts/optional/greeter.nix
+++ hosts/optional/greeter.nix
@@ -19,12 +19,35 @@ let
   ''}";
 in
 {
+  users.extraUsers.greeter = {
+    packages = [
+      gtkTheme.package
+      iconTheme.package
+    ];
+    # For caching and such
+    home = "/tmp/greeter-home";
+    createHome = true;
+  };
+
   programs.regreet = {
     enable = true;
+
+    settings = {
+      GTK = {
+        icon_theme_name = "Papirus";
+        theme_name = gtkTheme.name;
+      };
+
+      background = {
+        path = wallpaper;
+        fit = "Cover";
+      };
+    };
   };
 
   services.greetd = {
     enable = true;
+    settings.default_session.command = sway-kiosk (lib.getExe config.programs.regreet.package);
   };
 
   services.xserver = {
