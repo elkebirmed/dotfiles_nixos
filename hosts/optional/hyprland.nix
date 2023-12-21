71272040b84ab0d1516507add8d2c94b33313431
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2023-12-21 14:08:11 +0100

    ...

--- hosts/optional/hyprland.nix
+++ hosts/optional/hyprland.nix
@@ -3,5 +3,6 @@
 {
   security.pam.services.swaylock = {};
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+  configPackages = [ inputs.hyprland.hyprland ];
   xdg.portal.enable = lib.mkDefault true;
 }
