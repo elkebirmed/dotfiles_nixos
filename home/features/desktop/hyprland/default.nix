e0decd4aea2706e88fc536790deeff946b7eb923
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2023-12-21 14:31:56 +0100

    ggg

--- home/features/desktop/hyprland/default.nix
+++ home/features/desktop/hyprland/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, config, ... }: {
   imports = [
     inputs.hyprland.homeManagerModules.default
   ];
@@ -10,6 +10,23 @@
   };
 
   wayland.windowManager.hyprland.settings = {
+    general = {
+      gaps_in = 15;
+      gaps_out = 20;
+      border_size = 2.7;
+      cursor_inactive_timeout = 4;
+      "col.active_border" = "0xff${config.colorscheme.colors.base0C}";
+      "col.inactive_border" = "0xff${config.colorscheme.colors.base02}";
+    };
+
+    group = {
+      "col.border_active" = "0xff${config.colorscheme.colors.base0B}";
+      "col.border_inactive" = "0xff${config.colorscheme.colors.base04}";
+      groupbar = {
+        font_size = 11;
+      };
+    };
+
     "$mod" = "SUPER";
     bind =
       [
