c01bfa5fb2666eb208331347d8d552b86139ff7a
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2023-12-21 12:14:46 +0100

    ...

--- home/features/desktop/hyprland/default.nix
+++ home/features/desktop/hyprland/default.nix
@@ -8,9 +8,4 @@
     xwayland.enable = true;
     systemd.enable = true;
   };
-
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gtk
-  ];
 }
--- hosts/optional/sddm.nix
+++ hosts/optional/sddm.nix
@@ -19,6 +19,11 @@
     };
   };
 
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal-gtk
+  ];
+
   services.xserver = {
     enable = true;
     layout = "fr";
